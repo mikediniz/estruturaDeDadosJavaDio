f8c1d9ed8b664ecc5ad1939500091c372f682268
mikediniz <mikediniz.system@gmail.com>
2023-04-17 23:56:07 -0300

    Crianção Projeto Fila

--- projetos/pilha/src/one/digitalinnovation/No.java
+++ projetos/pilha/src/one/digitalinnovation/No.java
@@ -1,2 +1,34 @@
-package one.digitalinnovation;public class No {
+package one.digitalinnovation;
+public class No {
+    private int dados;
+    private No refNo = null;
+
+    public No() {
+    }
+    public No(int dados) {
+        this.dados = dados;
+    }
+
+    public int getDados() {
+        return dados;
+    }
+
+    public void setDados(int dados) {
+        this.dados = dados;
+    }
+
+    public No getRefNo() {
+        return refNo;
+    }
+
+    public void setRefNo(No refNo) {
+        this.refNo = refNo;
+    }
+
+    @Override
+    public String toString() {
+        return "No{" +
+                "dados=" + dados +
+                '}';
+    }
 }
